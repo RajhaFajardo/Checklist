3800f9056fc60b22c3fa601739efde19e7ad1442
rajha <rajhafajardo@hotmail.com>
2020-03-03 13:51:11 -0300

    Initial Commit

---
+++ Checklist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by Rajha Fajardo on 03/03/20.
+//  Copyright © 2020 Rajha Fajardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
